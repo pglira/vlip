d467132d175670767b9fe1753cce333e043dd3a7
Philipp Glira <philipp.glira@gmail.com>
2026-05-04 08:33:41 +0200

    Add window / taskbar icon
    Bundles Lucide's film icon (MIT) as a Qt resource and applies it via
    QApplication::setWindowIcon so the window manager picks it up.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "MainWindow.hpp"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
     app.setApplicationName("vlip");
     app.setOrganizationName("vlip");
+    app.setWindowIcon(QIcon(":/vlip.svg"));
 
     vlip::MainWindow w;
     w.show();
